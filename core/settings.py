82d70926e9a152b2dddcf74ce557640dcf8169bc
NicDs <nikolasdurango@gmail.com>
2024-02-07 23:17:22 -0500

    django cors

--- core/settings.py
+++ core/settings.py
@@ -32,6 +32,7 @@ BASE_APPS = [
 
 THIRD_APPS = [
     'rest_framework',
+    'corsheaders',
 
 ]
 
@@ -46,11 +47,26 @@ INSTALLED_APPS = BASE_APPS + THIRD_APPS + OWN_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:8080',
+    'http://127.0.0.1:8080',
+]
+
+CORS_ALLOWED_METHODS = [
+    'GET',
+    'OPTIONS',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
 ]
 
 ROOT_URLCONF = 'core.urls'
